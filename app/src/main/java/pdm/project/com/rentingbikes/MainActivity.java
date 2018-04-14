01b876f061279f9172c58ca057e36ee02ba124a2
Claudia <klaudiahoxha21@yahoo.com>
2018-04-14 11:47:34 +0300

    I have added LocationActivity

--- app/src/main/java/pdm/project/com/rentingbikes/MainActivity.java
+++ app/src/main/java/pdm/project/com/rentingbikes/MainActivity.java
@@ -1,5 +1,6 @@
 package pdm.project.com.rentingbikes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,20 +78,15 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Intent intent;
+        if (id == R.id.nav_lista_locatii) {
+            intent = new Intent(MainActivity.this, LocationActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_trasee) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
